09ac90f4bfa6b2ef8dc1fb28246b70029502b2e5
KOITT <KOITT@KOITT-PC>
2018-11-28 20:55:38 +0900

    11-28 (8번 문제)

---
+++ src/main/java/com/test/sp/vo/NullTest.java
@@ -0,0 +1,17 @@
+package com.test.sp.vo;
+
+public class NullTest {
+
+	public static void main(String[] args) {
+
+		String test = null;
+
+		if (test.equals("test")) {
+
+			log.info("test => {}", test);
+
+		}
+
+	}
+
+}
